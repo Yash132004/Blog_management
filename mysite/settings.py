fa54b3c7838ac5080c31948590b77ca2dacf136f
Yash Ojha <yashojha60@gmail.com>
2026-01-11 19:50:57 +0530

    commit this for pythonanywhere.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-23eg3^&t%biw7sirzms&kgixt)+o*-z@q6x6j$fepkls#!3y)=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Yash01.pythonanywhere.com']
 
 
 # Application definition
