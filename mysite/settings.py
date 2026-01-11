57f799dd820926c3a72feb6e0bc9b0f86a630009
Yash Ojha <yashojha60@gmail.com>
2026-01-11 20:30:12 +0530

    back undo

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-23eg3^&t%biw7sirzms&kgixt)+o*-z@q6x6j$fepkls#!3y)=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['Yash01.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
